283fd23b59ce0d37fdff64358cd2d3735e8f0f33
kumkumhirve <kumkumhirve452@gmail.com>
2025-05-11 23:13:27 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+'''Q19. Write a program to accept n number from user and show how
+many numbers are even or odd.'''
+
+num = int(input("enter the number:"))
+if num%2==0:
+    print("number is even")
+
+else:
+    print("number is odd")
